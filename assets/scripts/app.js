664cac24addca64ec50850de446076153af73711
Lewis Vine <lew.vine@gmail.com>
2024-07-22 20:26:54 -0400

    Added reset and const functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,34 +3,65 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100;
+const MODE_ATTACK = 'ATTACK';
+const MODE_STRONG_ATTACK = 'STRONG ATTACK';
+
+const enteredValue = prompt('Maximum life for you and the monster.', '100');
+
+let chosenMaxLife = parseInt(enteredValue);
+
+if(isNaN(chosenMaxLife) || chosenMaxLife <= 0){
+    chosenMaxLife = 100;
+}
+
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
+function reset(){
+    currentMonsterHealth = chosenMaxLife;
+    currentPlayerHealth = chosenMaxLife;
+    resetGame(chosenMaxLife);
+}
+
 function endRound(){
+    console.log(`Current player health: ${currentPlayerHealth}`);
+    console.log(`Current monster health: ${currentMonsterHealth}`);
+    const initialPlayerHealth = currentPlayerHealth;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
 
+    if(currentPlayerHealth <= 0 && hasBonusLife){
+        hasBonusLife = false;
+        removeBonusLife();
+        currentPlayerHealth = initialPlayerHealth;
+        alert('You would be dead, but the bonus life saved you!');
+        setPlayerHealth(initialPlayerHealth);
+    }
+
     if(currentMonsterHealth <= 0 && currentPlayerHealth > 0){
         alert("You won!");
+        reset();
     } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0){
         alert("You lost!");
+        reset();
     } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0){
         alert("You have a draw!");
+        reset();
     }
 }
 
 function attackMonster(mode){
     let maxDamage;
-    if(mode === 'ATTACK'){
+    if(mode === MODE_ATTACK){
         maxDamage = ATTACK_VALUE;
-    } else{
+    } else if (mode === STRONG_ATTACK){
         maxDamage = STRONG_ATTACK_VALUE;
     }
-    dealMonsterDamage(maxDamage);
-    currentMonsterHealth -= maxDamage;
+    const damage = dealMonsterDamage(maxDamage);
+    currentMonsterHealth -= damage;
     endRound();
 }
 
